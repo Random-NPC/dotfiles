dde27a3b9681af97e3b01e6587a09d5302110e8e
Random-NPC <korrafan72@gmail.com>
2024-06-28 15:45:32 +0300

    Testing

---
+++ nvim/lua/plugins/ui.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+      config = function()
+vim.keymap.set("n", "<C-n>", ":Neotree toggle left")
+   end
+},
+{
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+  end,
+  opts = {}
+},
+{
+    'nvim-lualine/lualine.nvim',
+    config = function()
+      require("lualine").setup({
+        opts = {
+          theme = "catppuccin"
+        }
+      })
+    end
+  }
+}
